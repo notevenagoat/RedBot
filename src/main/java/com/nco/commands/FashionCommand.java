b8f75c2c008ee56a221c3735a82edc3513498617
notevenagoat <itinoco@pacificstream.com.au>
2021-06-22 14:35:04 +1000

    MAde the fashoin roll randoms from the list and put it in ! yay. generated loclly

--- src/main/java/com/nco/commands/FashionCommand.java
+++ src/main/java/com/nco/commands/FashionCommand.java
@@ -11,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 
 public class FashionCommand extends AbstractCommand {
 
@@ -20,8 +22,18 @@ public class FashionCommand extends AbstractCommand {
 
     @Override
     protected void returnHelp() {
-        String[] fashion = {"Random 1", "Random 2", "Random 3", "Random 4", "Random 5", "Random 6", "Random 7"};
+//        String[] fashion = {"Random 1", "Random 2", "Random 3", "Random 4", "Random 5", "Random 6", "Random 7"};
+        String[] clothingStyles = {"Bag Lady Chic", "Gang Colors", "Generic Chic", "Bohemian", "Leisurewear", "Nomad Leathers", "Asia Pop", "Urban Flash", "Businesswear"};
+
+        String[] fashion = new String[7];
+
+        for (int i = 0; i < 7; i++)
+        {
+            int random_int = (int)Math.floor(Math.random()*(8+1)+0);
+            fashion[i]=clothingStyles[random_int];
+        }
         EmbedBuilder info = new EmbedBuilder();
+
         info.setTitle("Fashion of the Week");
         info.setDescription("Name of stlyle.");
         info.addField("Bottoms", fashion[0], true);
@@ -36,4 +48,5 @@ public class FashionCommand extends AbstractCommand {
         channel.sendMessage(info.build()).queue();
         info.clear();
     }
+
 }
